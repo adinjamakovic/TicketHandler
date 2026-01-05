3d0c73e34de8782764e7b9d0c2105c0a31688fff
adinjamakovic <adinjamakovic2004@gmail.com>
2026-01-05 20:02:26 +0100

    Added navbar

--- Market.Frontend/TicketHandler-frontend/src/app/modules/shared/shared-module.ts
+++ Market.Frontend/TicketHandler-frontend/src/app/modules/shared/shared-module.ts
@@ -8,7 +8,7 @@ import { FitConfirmDialogComponent } from './components/fit-confirm-dialog/fit-c
 import {DialogHelperService} from './services/dialog-helper.service';
 import { FitLoadingBarComponent } from './components/fit-loading-bar/fit-loading-bar.component';
 import { FitTableSkeletonComponent } from './components/fit-table-skeleton/fit-table-skeleton.component';
-
+import { FitDatepickerComponent } from './components/fit-datepicker/fit-datepicker.component';
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { FitTableSkeletonComponent } from './components/fit-table-skeleton/fit-t
     FitPaginatorBarComponent,
     FitConfirmDialogComponent,
     FitLoadingBarComponent,
-    FitTableSkeletonComponent
+    FitTableSkeletonComponent,
+    FitDatepickerComponent
   ],
   imports: [
     CommonModule,
